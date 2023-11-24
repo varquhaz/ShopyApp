843d579d0cee3093973cc9f412ca5b911655db11
varquhaz <juanjosevar02@gmail.com>
2023-11-24 16:32:09 -0400

    Added Filter by Category & Sort By

--- app/src/main/java/com/varqulabs/shopyapp/presentation/home/HomeEvent.kt
+++ app/src/main/java/com/varqulabs/shopyapp/presentation/home/HomeEvent.kt
@@ -2,7 +2,6 @@ package com.varqulabs.shopyapp.presentation.home
 
 sealed class HomeEvent {
     data object ReloadNetwork : HomeEvent()
-    data object Dismiss : HomeEvent()
     data class SortByFilter(val sortFilter: String) : HomeEvent()
     data class FilterByCategory(val category: String) : HomeEvent()
 
